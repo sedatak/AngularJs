d11837bdc27e6107ed0c8b65134a46edde20a09f
“sedatak” <sedat.ak.bm@gmail.com>
2017-02-24 23:40:31 +0200

    Yönlendirme uygulaması eklendi

---
+++ routing-example/script.js
@@ -0,0 +1,38 @@
+	// routeApp adında bir modül oluşturuldu
+	var routeApp = angular.module('routeApp', ['ngRoute']);
+
+	// yönlendirme ayarları
+	routeApp.config(function($routeProvider) {
+		$routeProvider
+
+			// anasayfa için yönlendirme
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'mainController'
+			})
+
+			// hakkında sayfası için yönlendirme
+			.when('/about', {
+				templateUrl : 'pages/about.html',
+				controller  : 'aboutController'
+			})
+
+			// iletişim sayfası için yönlendirme
+			.when('/contact', {
+				templateUrl : 'pages/contact.html',
+				controller  : 'contactController'
+			});
+	});
+
+	routeApp.controller('mainController', function($scope) {
+		// view'da görüntülenecek mesaj
+		$scope.message = 'Anasayfa Mesajı';
+	});
+
+	routeApp.controller('aboutController', function($scope) {
+		$scope.message = 'Hakkında Mesajı';
+	});
+
+	routeApp.controller('contactController', function($scope) {
+		$scope.message = 'İletişim Mesajı';
+	});
